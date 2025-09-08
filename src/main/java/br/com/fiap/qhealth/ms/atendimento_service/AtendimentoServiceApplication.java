da866278b7a651c6a02476a2d3c8bf672fc9442e
dsouzajm <danielsouza.jm@gmail.com>
2025-09-08 15:33:23 -0300

    Estruturacao inicial.

---
+++ src/main/java/br/com/fiap/qhealth/ms/atendimento_service/AtendimentoServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.qhealth.ms.atendimento_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtendimentoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtendimentoServiceApplication.class, args);
+	}
+
+}
