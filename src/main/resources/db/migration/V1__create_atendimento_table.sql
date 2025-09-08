6af9337c0b7f77cd4d0ade922286c77776a809e8
dsouzajm <danielsouza.jm@gmail.com>
2025-09-08 14:58:38 -0300

    Estruturacao inicial.

---
+++ src/main/resources/db/migration/V1__create_atendimento_table.sql
@@ -0,0 +1,28 @@
+-- Criação da tabela de fila
+CREATE TABLE fila (
+    id UUID PRIMARY KEY,
+    idUnidadeSaude UUID NOT NULL,
+    nomeFila VARCHAR(255) NOT NULL,
+    tipoFila VARCHAR(255) NOT NULL,
+    data_criacao TIMESTAMP NOT NULL DEFAULT NOW(),
+    data_ultima_alteracao TIMESTAMP
+);
+
+-- Criação da tabela de itens do pedido
+CREATE TABLE atendimento (
+    id UUID PRIMARY KEY,
+    idPaciente UUID NOT NULL,
+    idFila UUID NOT NULL,
+    posicaoFila INTEGER NOT NULL,
+    idAnamnese UUID NOT NULL,
+    data_criacao TIMESTAMP NOT NULL DEFAULT NOW(),
+    data_ultima_alteracao TIMESTAMP,
+
+    CONSTRAINT fk_fila
+        FOREIGN KEY (idFila)
+        REFERENCES fila (id)
+);
+
+-- Indexes para performance
+CREATE INDEX idx_fila ON fila (id);
+CREATE INDEX idx_atendimento ON fila (atendimento);
